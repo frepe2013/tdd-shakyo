1934043bb53b8873e06358f0b6101529ad2b2a92
frepe2013 <needvitneedfgt@gmail.com>
2017-11-03 13:08:46 +0900

    p.20

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -7,12 +7,17 @@ import org.junit.jupiter.api.Test;
 public class MoneyTest {
 
 	@Test
-	void testMultiplication() {
+	public void testMultiplication() {
 		Dollar five = new Dollar(5);
 		Dollar product = five.times(2);
 		assertEquals(10, product.amount);
 		product = five.times(3);
 		assertEquals(15, product.amount);
+	}
+
+	@Test
+	public void testEquality() {
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
 	}
 
 }
