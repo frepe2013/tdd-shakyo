a55bf1a45c9e42ac829f55190a5c09db6c640e63
frepe2013 <needvitneedfgt@gmail.com>
2017-11-09 12:30:58 +0900

    p.58.1

--- src/money/Money.java
+++ src/money/Money.java
@@ -4,9 +4,13 @@ public abstract class Money {
 
 	protected int amount;
 
+	protected String currency;
+
 	abstract Money times(int multiplier);
 
-	abstract String currency();
+	String currency() {
+		return currency;
+	}
 
 	public boolean equals(Object object) {
 		Money money = (Money)object;
