caa68e02758352f90fe25ac689deb503128d6e6e
frepe2013 <needvitneedfgt@gmail.com>
2017-11-09 12:29:00 +0900

    p.57

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -1,13 +1,15 @@
 package money;
 
 public class Dollar extends Money {
+	private String currency;
 	Dollar(int amount){
 		this.amount = amount;
+		currency = "USD";
+	}
+	String currency() {
+		return currency;
 	}
 	Money times(int multiplier) {
 		return new Dollar(amount * multiplier);
-	}
-	String currency() {
-		return "USD";
 	}
 }
